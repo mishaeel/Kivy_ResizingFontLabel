3b2637a94f2b84c45652073db7ba6796d1695a78
mishaeel <67398902+mishaeel@users.noreply.github.com>
2023-12-14 20:55:15 -0800

    Update FontResizingLabel.py

--- FontResizingLabel.py
+++ FontResizingLabel.py
@@ -8,7 +8,7 @@ to another display. I have only tested on Windows and desktop,
 float and grid layouts. Feel free to let me know if you experience issues.
 
 To use the code, you have to declare the inital font size in python. 
-For whatever reason, declaring the iniial size in the kv language has not 
+For whatever reason, declaring the initial size in the kv language has not 
 worked for me. You also have to use 'sp' units in your font size.
 
 Either: 1. Add the code below to your own Label, or
@@ -16,17 +16,17 @@ Either: 1. Add the code below to your own Label, or
         2. Save the code as a module in your app's root directory
            and include as follows in your application's python code.
            ---------------------------------------------------------
-			from FontResizingLabel import FontResizingLabel
+		from FontResizingLabel import FontResizingLabel
 
-			class YourLabel(FontResizingLabel):
+		class YourLabel(FontResizingLabel):
 
-				original_font_size = '22sp'
-				
-				# declare your initial font size in python
-				# as a string with sp units
+			original_font_size = '22sp'
+			
+			# declare your initial font size in python
+			# as a string with sp units
 
-				def __init__(self, **kwargs):
-					super(YourLabel, self).__init__(**kwargs)
+			def __init__(self, **kwargs):
+				super(YourLabel, self).__init__(**kwargs)
 '''
 
 class FontResizingLabel(Label):
